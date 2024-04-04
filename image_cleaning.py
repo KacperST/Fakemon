2b784449322e77c139cf7d2cb9ed25d5dca4212b
KacperSt <kstankiewicz@student.agh.edu.pl>
2024-04-04 19:41:48 +0200

    Clear notebook

--- image_cleaning.py
+++ image_cleaning.py
@@ -4,46 +4,33 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def move_images_to_different_director(image: np.array, output_dir: str, file:str, word: str) -> None:
-    file_name, file_extension = os.path.splitext(file)
-    new_file_name = file_name + f"_{word}" + file_extension
-    output_path = os.path.join(output_dir, new_file_name)
-    cv2.imwrite(output_path, image)
-         
-def change_white_background_to_black() -> None:
-    current_dir = os.getcwd()
-    dataset_path = f"{current_dir}/pokemons"
-    for file in os.listdir(dataset_path):
-        file_path = os.path.join(dataset_path, file)
-        if file.endswith(".jpg"):
-            image = cv2.imread(file_path)
-            
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-            _, binary = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY_INV)
-
-            contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-            mask = np.zeros_like(image)
-
-            cv2.drawContours(mask, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
-
-            result = cv2.bitwise_and(image, mask)
-
-            move_images_to_different_director(result, f"{current_dir}/pokemons_black_background", file, "")
-            
-
-
-def resize_images2(dataset_path: str,output_dir_path:str, image_size:Tuple[int, int] =(256, 256)) -> None:
+def change_white_background_to_black(dataset_path: str, output_dir_path:str) -> None:
     # create output directory if it does not exist
     if not os.path.exists(output_dir_path):
         os.makedirs(os.path.join(os.getcwd(), output_dir_path))
     for file in os.listdir(dataset_path):
         file_path = os.path.join(dataset_path, file)
         I = cv2.imread(file_path)
+        gray = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+        _, binary = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY_INV)
+        contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        mask = np.zeros_like(I)
+        cv2.drawContours(mask, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
+        result = cv2.bitwise_and(I, mask)
+        file_name, file_extension = os.path.splitext(file)
+        new_file_name = file_name + f"_black_background" + file_extension
+        output_path = os.path.join(output_dir_path, new_file_name)
+        cv2.imwrite(output_path, result)
+       
+
+
+def resize_images2(dataset_path: str,output_dir_path:str, image_size:Tuple[int, int] =(256, 256)) -> None:
+    for file in os.listdir(dataset_path):
+        file_path = os.path.join(dataset_path, file)
+        I = cv2.imread(file_path)
         I_resized = cv2.resize(I, image_size)
         file_name, file_extension = os.path.splitext(file)
-        new_file_name = file_name + f"_resized{image_size[0]}" + file_extension
+        new_file_name = file_name + file_extension
         output_path = os.path.join(output_dir_path, new_file_name)
         cv2.imwrite(output_path, I_resized)
 
@@ -85,7 +72,8 @@ def add_noise2(dataset_path: str, output_dir_path:str) -> None:
 
 
 def pipeline(dataset_path: str, output_dir_path:str) -> None:
-    resize_images2(dataset_path, output_dir_path, (256, 256))
+    change_white_background_to_black(dataset_path, output_dir_path)
+    resize_images2(output_dir_path, output_dir_path, (256, 256))
     add_reflection(output_dir_path, output_dir_path)
     change_contrast(output_dir_path, output_dir_path, 1.3, 0)
     add_noise2(output_dir_path, output_dir_path)
@@ -93,9 +81,11 @@ def pipeline(dataset_path: str, output_dir_path:str) -> None:
 
 def main() -> None:
     current_dir = os.getcwd()
-    dataset_path = f"{current_dir}/pokemons_contrast"
+    dataset_path = f"{current_dir}/pokemons"
     output_dir = f"{current_dir}/pokemons_noise"
     pipeline(dataset_path, output_dir)
+    
+    
 if __name__ == "__main__":
     main()
     
