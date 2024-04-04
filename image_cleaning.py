d0abe23c974cae6ed14e281822398c52c788dd6b
Marcin <81770791+MarcinZ20@users.noreply.github.com>
2024-04-04 21:16:49 +0200

    Update image_cleaning.py

--- image_cleaning.py
+++ image_cleaning.py
@@ -2,13 +2,25 @@ import os
 from typing import Tuple
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+def check_size(dataset_path: str) -> None:
+    for file in tqdm(os.listdir(dataset_path)):
+        file_path = os.path.join(dataset_path, file)
+        img = cv2.imread(file_path)
+
+        width, height, _ = img.shape
+
+        if width < 256 or height < 256:
+            os.remove(file_path)
+
+    print(f"\nChecking size is done.\nRemaining images: {len(os.listdir(dataset_path))}\n")
 
 def change_white_background_to_black(dataset_path: str, output_dir_path:str) -> None:
     # create output directory if it does not exist
     if not os.path.exists(output_dir_path):
         os.makedirs(os.path.join(os.getcwd(), output_dir_path))
-    for file in os.listdir(dataset_path):
+    for file in tqdm(os.listdir(dataset_path)):
         file_path = os.path.join(dataset_path, file)
         I = cv2.imread(file_path)
         gray = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
@@ -18,14 +30,15 @@ def change_white_background_to_black(dataset_path: str, output_dir_path:str) -> 
         cv2.drawContours(mask, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
         result = cv2.bitwise_and(I, mask)
         file_name, file_extension = os.path.splitext(file)
-        new_file_name = file_name + f"_black_background" + file_extension
+        new_file_name = file_name + f"_black_background" + ".jpg"
         output_path = os.path.join(output_dir_path, new_file_name)
         cv2.imwrite(output_path, result)
+    
+    print("\nBackgrounds changed to black.\n")
        
 
-
 def resize_images2(dataset_path: str,output_dir_path:str, image_size:Tuple[int, int] =(256, 256)) -> None:
-    for file in os.listdir(dataset_path):
+    for file in tqdm(os.listdir(dataset_path)):
         file_path = os.path.join(dataset_path, file)
         I = cv2.imread(file_path)
         I_resized = cv2.resize(I, image_size)
@@ -33,19 +46,24 @@ def resize_images2(dataset_path: str,output_dir_path:str, image_size:Tuple[int, 
         new_file_name = file_name + file_extension
         output_path = os.path.join(output_dir_path, new_file_name)
         cv2.imwrite(output_path, I_resized)
+    
+    print("\nImages resized.\n")
 
 def change_contrast(dataset_path: str, output_dir_path:str, alpha: float, beta: float) -> None:
-    for file in os.listdir(dataset_path):
+    for file in tqdm(os.listdir(dataset_path)):
         file_path = os.path.join(dataset_path, file)
         I = cv2.imread(file_path)
         adjusted = cv2.convertScaleAbs(I, alpha=alpha, beta=beta)
         file_name, file_extension = os.path.splitext(file)
-        new_file_name = file_name + f"_contrast{alpha}" + file_extension
+        new_file_name = file_name + f"_contrast" + file_extension
         output_path = os.path.join(output_dir_path, new_file_name)
         cv2.imwrite(output_path, adjusted)
 
+    print("\nContrast added.\n")
+
+
 def add_reflection(dataset_path: str, output_dir_path:str) -> None:
-    for file in os.listdir(dataset_path):
+    for file in tqdm(os.listdir(dataset_path)):
         file_path = os.path.join(dataset_path, file)
         I = cv2.imread(file_path)
         I_reflected = cv2.flip(I, 1)  # Flip horizontally
@@ -53,25 +71,30 @@ def add_reflection(dataset_path: str, output_dir_path:str) -> None:
         new_file_name = file_name + f"_reflected" + file_extension
         output_path = os.path.join(output_dir_path, new_file_name)
         cv2.imwrite(output_path, I_reflected)
+    
+    print("\nReflections added.\n")
+
 
 def add_noise2(dataset_path: str, output_dir_path:str) -> None:
-    for file in os.listdir(dataset_path):
+    for file in tqdm(os.listdir(dataset_path)):
         file_path = os.path.join(dataset_path, file)
         I = cv2.imread(file_path)
         row, col, ch = I.shape
         mean = 0
-        var = 0.1
-        sigma = var ** 0.5
-        gauss = np.random.normal(mean, sigma, (row, col, ch))
+        std_dev = 5
+        gauss = np.random.normal(mean, std_dev, (row, col, ch))
         gauss = gauss.reshape(row, col, ch)
         noisy = I + gauss
         file_name, file_extension = os.path.splitext(file)
         new_file_name = file_name + f"_noisy" + file_extension
         output_path = os.path.join(output_dir_path, new_file_name)
         cv2.imwrite(output_path, noisy)
+    
+    print("\nNoise added.\n")
 
 
 def pipeline(dataset_path: str, output_dir_path:str) -> None:
+    check_size(dataset_path)
     change_white_background_to_black(dataset_path, output_dir_path)
     resize_images2(output_dir_path, output_dir_path, (256, 256))
     add_reflection(output_dir_path, output_dir_path)
