24baea81f0a8b0ac84ce4b3f8f03787b7896323e
KacperSt <kstankiewicz@student.agh.edu.pl>
2024-04-09 23:06:04 +0200

    Improve names in image_cleaning and add dataset

--- image_cleaning.py
+++ image_cleaning.py
@@ -37,7 +37,7 @@ def change_white_background_to_black(dataset_path: str, output_dir_path:str) -> 
     print("\nBackgrounds changed to black.\n")
        
 
-def resize_images2(dataset_path: str,output_dir_path:str, image_size:Tuple[int, int] =(256, 256)) -> None:
+def resize_images(dataset_path: str,output_dir_path:str, image_size:Tuple[int, int] =(256, 256)) -> None:
     for file in tqdm(os.listdir(dataset_path)):
         file_path = os.path.join(dataset_path, file)
         I = cv2.imread(file_path)
@@ -75,7 +75,7 @@ def add_reflection(dataset_path: str, output_dir_path:str) -> None:
     print("\nReflections added.\n")
 
 
-def add_noise2(dataset_path: str, output_dir_path:str) -> None:
+def add_noise(dataset_path: str, output_dir_path:str) -> None:
     for file in tqdm(os.listdir(dataset_path)):
         file_path = os.path.join(dataset_path, file)
         I = cv2.imread(file_path)
@@ -115,10 +115,10 @@ def add_negative(dataset_path: str, output_dir_path: str, image_size:Tuple[int, 
 def pipeline(dataset_path: str, output_dir_path:str) -> None:
     check_size(dataset_path)
     change_white_background_to_black(dataset_path, output_dir_path)
-    resize_images2(output_dir_path, output_dir_path, (256, 256))
+    resize_images(output_dir_path, output_dir_path, (256, 256))
     add_reflection(output_dir_path, output_dir_path)
     change_contrast(output_dir_path, output_dir_path, 1.3, 0)
-    add_noise2(output_dir_path, output_dir_path)
+    add_noise(output_dir_path, output_dir_path)
     add_negative(dataset_path, output_dir_path)
 
 
@@ -126,7 +126,7 @@ def pipeline(dataset_path: str, output_dir_path:str) -> None:
 def main() -> None:
     current_dir = os.getcwd()
     dataset_path = f"{current_dir}/pokemons"
-    output_dir = f"{current_dir}/pokemons_noise"
+    output_dir = f"{current_dir}/pokemons_dataset"
     pipeline(dataset_path, output_dir)
     
     
